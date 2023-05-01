be842691b545f74be669e6ecc3b5f05e463718c5
yangwenfeng <yangwenfeng.0719@bytedance.com>
2023-05-01 15:00:22 +0800

    chore: update playground

--- playground/src/utils/plasticine-monitor.ts
+++ playground/src/utils/plasticine-monitor.ts
@@ -1,5 +1,14 @@
 import { init, xhrSender } from '@plasticine-monitor-sdk/browser'
 
+import pkg from '../../package.json'
+
 export function setupPlasticineMonitor() {
-  init({ url: '', projectId: '', enableLogger: true, sender: xhrSender })
+  init({
+    url: 'http://localhost:8868/api/v1/browser-event',
+    projectId: 'abc123',
+    enableLogger: true,
+    sender: xhrSender,
+    env: import.meta.env.MODE,
+    release: pkg.version,
+  })
 }
